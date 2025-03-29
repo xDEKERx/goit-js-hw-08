9b1690f397ad08f1e8a5c7237d7e93e3044b1ff0
Dmytro Deker <prostodeker@gmail.com>
2025-03-29 17:09:45 +0200

    V2

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const list = document.querySelector('#categories');
-console.log(`Number of categories: ${list.children.length}`);
-
-const item = document.querySelectorAll('.item');
-item.forEach(element => {
-  console.log(`Category: ${element.firstElementChild.textContent}`);
-  console.log(`Element: ${element.lastElementChild.children.length}`);
-});
